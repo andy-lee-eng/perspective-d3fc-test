1daef68a4f29c312317e41ef3edbe25e5cc0545f
Matthew Hooper <mhooper@scottlogic.com>
2019-03-15 10:12:51 +0000

    Add a split-by OHLC example and re-order the values to Open, Close, High, Low

---
+++ public/Investing/investing2.js
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+
+window.addEventListener("WebComponentsReady", function () {
+    var url = '../data/shares.csv';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        var table = perspective.worker().table(xhr.response);
+        var elem = document.getElementsByTagName("perspective-viewer")[0];
+        elem.load(table);
+    }
+
+    xhr.send(null);
+});
